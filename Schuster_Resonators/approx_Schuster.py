dce31b645c4bddc2d1e703eb27a6a6425578b40e
cbelo <cbelo.miguel@gmail.com>
2024-04-15 11:37:27 +0200

    schuster approx working

--- Schuster_Resonators/approx_Schuster.py
+++ Schuster_Resonators/approx_Schuster.py
@@ -28,6 +28,56 @@ def cap_coupling(width_cap, length_horizontal, distance_to_feedline,epsilon_r, t
     cap = 2*pyc.epsilon_0*(epsilon_r-1)*K1/K1_prime +4*pyc.epsilon_0*K0/K0_prime
     return cap*length_horizontal/2
 
+def cap_coupling_new(width_cap, length_horizontal, distance_to_feedline,epsilon_r, thickness_subs, width_feedline):
+    
+    #Define simpler variables
+    s = distance_to_feedline
+    w1 = width_cap
+    w2 = width_feedline
+    h = thickness_subs
+
+    #Calculate the k parameters   
+    k0 = np.sqrt(s*(w1+w2+s)/((s+w1)*(s+w2)))
+    k0_prime = np.sqrt(1-k0**2)
+    K0 = ellipk(k0)
+    K0_prime = ellipk(k0_prime)
+    num = (np.exp(2*np.pi*(w1+s)/h) -np.exp(2*np.pi*w1/h)) * (np.exp(2*np.pi*(w1+w2+s)/h) -1)
+    den = (np.exp(2*np.pi*(w1+w2+s)/h) -np.exp(2*np.pi*w1/h)) * (np.exp(2*np.pi*(w1+s)/h) -1)
+    k1 = np.sqrt(num/den)
+    k1_prime = np.sqrt(1-k1**2)
+    K1 = ellipk(k1)
+    K1_prime = ellipk(k1_prime)
+
+    #Calculate the capacitance
+    C0 = 2*pyc.epsilon_0*K0_prime/K0
+    C1 = pyc.epsilon_0*(epsilon_r-1)*K1_prime/K1
+    return (C0 + C1)*length_horizontal
+
+def cap_ground_new(width_cap, length_vertical, distance_to_ground,epsilon_r, thickness_subs):
+    #Define simpler variables
+    s = distance_to_ground
+    w = width_cap
+    h = thickness_subs
+
+    #Calculate the k parameters   
+    k0 = np.sqrt(w/(w+s))
+    k0_prime = np.sqrt(1-k0**2)
+    K0 = ellipk(k0)
+    K0_prime = ellipk(k0_prime)
+    num = np.exp(np.pi*w/h) -1
+    den = np.exp(np.pi*(w+s)/h) -1
+    k1 = np.sqrt(num/den)
+    k1_prime = np.sqrt(1-k1**2)
+    K1 = ellipk(k1)
+    K1_prime = ellipk(k1_prime)
+
+    #Calculate the capacitance
+    C0 = 2*pyc.epsilon_0*K0_prime/K0
+    C1 = pyc.epsilon_0*(epsilon_r-1)*K1_prime/K1
+    # print('Length',length_vertical)
+
+    return (C0 + C1)*length_vertical*2 #The factor of 2 is because the ground plane is on both sides of the resonator
+
 def cap_ground(width_cap, length_vertical, distance_to_ground,epsilon_r, thickness_subs):
     '''
     Returns the coupling capacitance of the Schuster rsonator to the ground plane
@@ -77,6 +127,18 @@ def impedance_Schuster(width_cap, length_horizontal, distance_to_feedline, width
     imp0 = np.sqrt(ind_ground_val / (cap_coupling_val + cap_ground_val))
     return imp0
 
+def impedance_Schuster_new(width_cap, length_horizontal_cap, length_vertical_cap, distance_to_feedline, width_ind, length_ind, distance_to_ground_cap, epsilon_r, thickness_subs, ind_kin_sq, width_feedline):
+    '''
+    Returns the impedance of the Schuster resonator
+    '''
+    cap_coupling_val = cap_coupling_new(width_cap, length_horizontal_cap, distance_to_feedline,epsilon_r, thickness_subs, width_feedline)
+    cap_ground_val = cap_ground_new(width_cap, length_vertical_cap, distance_to_ground_cap,epsilon_r, thickness_subs)
+    
+    distance_to_ground_ind = distance_to_ground_cap + length_horizontal_cap/2+ width_cap/2
+    ind_ground_val = ind_ground_total(width_ind, length_ind, distance_to_ground_ind, ind_kin_sq)
+    imp0 = np.sqrt(ind_ground_val / (cap_coupling_val + cap_ground_val))
+    return imp0
+
 def resonance_freq_Schuster(width_cap, length_horizontal, distance_to_feedline, width_ind, length_ind, distance_to_ground_cap, epsilon_r, thickness_subs, ind_kin_sq):
     '''
     Returns the resonance frequency of the Schuster resonator
@@ -85,6 +147,20 @@ def resonance_freq_Schuster(width_cap, length_horizontal, distance_to_feedline, 
     cap_ground_val = cap_ground(width_cap, length_ind, distance_to_ground_cap,epsilon_r, thickness_subs)
     distance_to_ground_ind = distance_to_ground_cap + length_horizontal/2 + width_cap/2
     print(length_ind)
+    ind_ground_val = ind_ground_total(width_ind, length_ind, distance_to_ground_ind, ind_kin_sq)
+    print(f'{ind_ground_val*1e9} nH')
+    print(f'{1e15*cap_ground_val} fF')
+    print(f'{1e15*cap_coupling_val} fF')
+    f0 = 1/(2*np.pi*np.sqrt(ind_ground_val*(cap_coupling_val + cap_ground_val)))
+    return f0
+
+def resonance_freq_Schuster_new(width_cap, length_horizontal_cap, length_vertical_cap, distance_to_feedline, width_ind, length_ind, distance_to_ground_cap, epsilon_r, thickness_subs, ind_kin_sq, width_feedline):
+    '''
+    Returns the resonance frequency of the Schuster resonator
+    '''
+    cap_coupling_val = cap_coupling_new(width_cap, length_horizontal_cap, distance_to_feedline,epsilon_r, thickness_subs, width_feedline)
+    cap_ground_val = cap_ground_new(width_cap, length_vertical_cap, distance_to_ground_cap,epsilon_r, thickness_subs)
+    distance_to_ground_ind = distance_to_ground_cap + length_horizontal_cap/2 + width_cap/2
     ind_ground_val = ind_ground_total(width_ind, length_ind, distance_to_ground_ind, ind_kin_sq)
     print(f'{ind_ground_val*1e9} nH')
     print(f'{1e15*cap_ground_val} fF')
